f7a8b2bf3a559a74c9518d4f143541a9ced9e519
Ankit jain <83234209+jain009@users.noreply.github.com>
2025-02-06 23:44:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function makeid(l) {
   // write your code here
 	let res = "";
 	let charList = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890"
-	for( let i = 0; i< l i++){
+	for( let i = 0; i< l; i++){
 		res  += charList.charAt {
 			Math.floor{
 			Math.random()*charList.length;
