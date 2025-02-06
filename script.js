090dc72777f43f6fe6b10360798a258622d8f4cc
Ankit jain <83234209+jain009@users.noreply.github.com>
2025-02-06 23:33:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
 function makeid(l) {
   // write your code here
+	let res = "";
+	let charList = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890"
+	for( let i = 0; i< l i++){
+		res  += charList.charAt {
+			Math.floor{
+			Math.random()*charList.length;
+				
+			}
+			
+		}
+		
+	}
+	return res;
 }
 
 // Do not change the code below.
-const l = prompt("Enter a number.");
+// const l = prompt("Enter a number.");
 alert(makeid(l));
